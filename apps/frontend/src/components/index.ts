5755ff57edc6f76e3a99178f44945557e0ca8b31
Jinming7 <411263626@qq.com>
2026-02-28 22:38:53 +0800

    chore: update component index with all exports

--- apps/frontend/src/components/index.ts
+++ apps/frontend/src/components/index.ts
@@ -9,11 +9,14 @@ export { default as Badge } from './Badge'
 export { default as Avatar, AvatarGroup } from './Avatar'
 export { default as Breadcrumb } from './Breadcrumb'
 export { default as ShareButton } from './ShareButton'
+export { default as Tooltip, HelpTooltip, InfoBadge } from './Tooltip'
 
 // Data Display
 export { default as RatingDistribution } from './RatingDistribution'
 export { default as VersionHistory } from './VersionHistory'
 export { default as ComparePanel, CompareTable } from './ComparePanel'
+export { default as StatCard, StatGrid } from './StatCard'
+export { default as DataTable, Table } from './DataTable'
 
 // Feedback
 export { default as Loading, AppCardSkeleton, AppGridSkeleton } from './Loading'
@@ -21,3 +24,29 @@ export { default as EmptyState } from './EmptyState'
 export { ErrorBoundary } from './ErrorBoundary'
 export { default as NotificationCenter } from './NotificationCenter'
 export { ToastProvider, useToast } from './Toast'
+
+// Navigation
+export { default as Pagination, SimplePagination, LoadMore } from './Pagination'
+export { default as Tabs, TabList } from './Tabs'
+
+// Overlays
+export { default as Modal, ConfirmModal } from './Modal'
+
+// Forms
+export { Input, Textarea, FormGroup, FormActions } from './Input'
+export { default as Select, MultiSelect } from './Select'
+export { default as Switch, Checkbox, Radio, RadioGroup } from './Switch'
+
+// Progress
+export { default as ProgressBar, CircularProgress } from './ProgressBar'
+
+// Skeleton
+export {
+  Skeleton,
+  TextSkeleton,
+  AvatarSkeleton,
+  CardSkeleton,
+  TableSkeleton,
+  ListSkeleton,
+  StatsSkeleton,
+} from './Skeleton'
